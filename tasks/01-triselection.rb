b0250eda9667848529defc0e8b165da7e7bc6d72
Michael <michaelbradley2004@gmail.com>
2021-03-25 10:31:42 -0400

    helo

---
+++ tasks/01-triselection.rb
@@ -0,0 +1,40 @@
+require 'byebug'
+
+arr = []
+
+9000.times do 
+	arr << rand(1..100)
+end
+
+debut = Time.now
+
+ii = 0
+j = 1
+tmp = 0
+
+puts arr
+
+puts "---------------"
+len = arr.length
+
+while ii < len -1
+	j = ii+1
+	plus_petit=ii
+	while j<(len)
+		if arr[plus_petit] > arr[j]
+			plus_petit=j
+		end
+		j=j+1
+	end
+	tmp = arr[ii]
+	arr[ii]=arr[plus_petit]
+	arr[plus_petit]=tmp
+
+	ii = ii+1
+end
+puts arr
+
+fin = Time.now
+
+diff = fin-debut
+puts diff
---
+++ tasks/test.rb
@@ -0,0 +1,40 @@
+require 'byebug'
+
+
+arr = []
+
+9000.times do
+	arr << rand(1..100)
+end
+
+debut = Time.now
+
+ii = 0
+j = 1
+tmp = 0
+
+
+
+len = arr.length
+
+
+while ii < len
+	j = 1
+	while j < (len - ii)
+		
+
+		if arr[j-1] < arr[j]
+			tmp = arr[j]
+			arr[j] = arr[j-1]
+			arr[j-1] = tmp
+		end
+		j = j+1
+	end
+	ii = ii+1
+end
+
+fin = Time.now
+
+diff = fin-debut
+
+puts diff
